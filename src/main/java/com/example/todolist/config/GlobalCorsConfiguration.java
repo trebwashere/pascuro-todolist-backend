195ab990e617594dee3ac9b50c885a50b0a2fd30
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-19 13:03:14 +0800

    [STToDoBackend][Robert] feat: Added cors config with allowed origins for localhost

---
+++ src/main/java/com/example/todolist/config/GlobalCorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.todolist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfiguration {
+    @Bean
+    public WebMvcConfigurer configureCors() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3003", "http://localhost:3002", "http://localhost:3001", "http://localhost:3000");
+            }
+        };
+    }
+}
